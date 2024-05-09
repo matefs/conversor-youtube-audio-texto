5814c12d72895263e878057b435c8e1d5c8a0457
Mateus Schverz <matefs8569@gmail.com>
2024-05-09 00:06:46 -0300

    refactor: removed unecessary comments

--- download_mp3_youtube.py
+++ download_mp3_youtube.py
@@ -8,12 +8,9 @@ t[0].download()
 print(t[0])
 
 
-# Listar todos os arquivos na pasta atual
 arquivos = os.listdir()
 
-# Iterar sobre os arquivos
 for arquivo in arquivos:
-    # Verificar se é um arquivo .mp4
     if arquivo.endswith('.mp4'):
         # Renomear para .mp3
         novo_nome = arquivo[:-4] + '.mp3'
