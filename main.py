83389f55006931b93784205fad4579ec87b42e81
Mateus Schverz <matefs8569@gmail.com>
2024-05-09 23:54:23 -0300

    broken change: tentando baixar o video pelo query params video_url

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, Request
+import pytube as pt
+import os
 
 app = FastAPI()
 
@@ -7,6 +9,18 @@ app = FastAPI()
 async def raiz(request: Request):
     video_url = request.query_params.get("video_url")
     if video_url is not None:
-        return {"video_url": f"{video_url}!"}
+        yt = pt.YouTube(str(video_url))
+        t = yt.streams.filter(only_audio=True)
+        t[0].download()
+
+        arquivos = os.listdir()
+
+        for arquivo in arquivos:
+            if arquivo.endswith('.mp4'):
+                # Renomear para .mp3
+                novo_nome = arquivo[:-4] + '.mp3'
+                os.rename(arquivo, novo_nome)
+
+        return {"video_url": f"{video_url}!", "status": "sucesso"}
     else:
         return {"mensagem": "Por favor, informe uma URL do YouTube no parâmetro /?video_url="}
